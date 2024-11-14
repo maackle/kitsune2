02a13051d83a65cb213f31262e22ec082e7a43c3
David Braden <neonphog@gmail.com>
2024-11-14 12:24:32 -0700

    add skeleton api crate (#1)
    * add skeleton api crate
    
    * address review comment

---
+++ crates/api/src/lib.rs
@@ -0,0 +1,5 @@
+#![deny(missing_docs)]
+//! Kitsune2 API contains kitsune module traits and the basic types required
+//! to define the api of those traits.
+//!
+//! If you want to use Kitsune2 itself, please see the kitsune2 crate.
