a2c20aadb737004168fd68a732352424f65335b3
Callum Dunster <cdunster@users.noreply.github.com>
2025-05-06 18:12:48 +0200

    chore: add Nix devShell and direnv support (#207)
    * chore(nix): remove unused variables
    
    * chore(nix): add devShell with Rust
    
    * chore: add direnv support
    
    * chore(nix): add required packages and env var to build

--- flake.nix
+++ flake.nix
@@ -15,10 +15,8 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem = { self', inputs', pkgs, system, ... }:
+      perSystem = { pkgs, system, ... }:
         let
-          inherit (pkgs) lib;
-
           rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
           craneLib = (crane.mkLib pkgs).overrideToolchain rust;
@@ -43,6 +41,19 @@
 
           packages = {
             inherit bootstrap-srv;
+          };
+
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                rust
+                perl
+                cmake
+                openssl
+              ];
+
+              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            };
           };
         };
     };
